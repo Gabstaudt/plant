5400f3a89c703dfcab064b0edfeed6e7928c888c
Gabstaudt <macstaudt@gmail.com>
2026-01-30 19:56:40 -0300

    feat: adição da tela de adicionar um novo sensor

---
+++ src/app/(app)/sensors/new/page.tsx
@@ -0,0 +1,9 @@
+import NewSensorForm from "@/app/components/sensors/new/NewSensorForm";
+
+export default function NewSensorPage() {
+  return (
+    <div className="p-4 md:p-6">
+      <NewSensorForm />
+    </div>
+  );
+}
