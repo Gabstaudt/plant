97299696e270cd4f7c15cebba3868e04e92f22fb
Gabstaudt <macstaudt@gmail.com>
2026-02-17 22:06:35 -0300

    Feat: direcionamenteo de página pós criação do sensor

--- src/app/(app)/plants/new/page.tsx
+++ src/app/(app)/plants/new/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import NewPlantForm from "@/app/components/plants/new/NewPlantForm";
+import { useRouter } from "next/navigation";
 
 export default function NewPlantPage() {
+  const router = useRouter();
   return (
     <div className="p-4 md:p-6">
-      <NewPlantForm />
+      <NewPlantForm onSubmitSuccess={() => router.push("/plants")} />
     </div>
   );
 }
